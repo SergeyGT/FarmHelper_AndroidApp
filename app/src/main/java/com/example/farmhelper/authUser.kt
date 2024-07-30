b18b14530bb42af1b66e9f798998055a77764f70
SergeyGT <sergejburdenuk35@gmail.com>
2024-07-30 04:16:05 +0300

    Добавлена проверка на наличие уже созданного аккаунта

--- app/src/main/java/com/example/farmhelper/authUser.kt
+++ app/src/main/java/com/example/farmhelper/authUser.kt
@@ -1,8 +1,14 @@
 package com.example.farmhelper
 
+import User
+
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +20,33 @@ class authUser : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_auth_user)
 
+        val loginUser: EditText = findViewById(R.id.Login_auth)
+        val passUser: EditText = findViewById(R.id.Password_auth)
+        val btnSign: Button = findViewById(R.id.buttonSign_auth)
+
         val registr: TextView = findViewById(R.id.registration)
+
+        btnSign.setOnClickListener {
+            val loginSign = loginUser.text.toString()
+            val passSign = passUser.text.toString()
+
+            val db = DBFarm(this, null)
+            val result = db.getUser(loginSign, passSign)
+
+            if (result) {
+               Toast.makeText(this, "Вы успешно вошли в аккаунт", Toast.LENGTH_LONG).show()
+                loginUser.text.clear()
+                passUser.text.clear()
+            } else {
+                Toast.makeText(this, "Данные аккаунта введены неверно или аккаунт не зарегистрирован",
+                    Toast.LENGTH_LONG).show()
+                loginUser.text.clear()
+                passUser.text.clear()
+                loginUser.setHintTextColor(Color.RED)
+                passUser.setHintTextColor(Color.RED)
+
+            }
+        }
 
         registr.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
